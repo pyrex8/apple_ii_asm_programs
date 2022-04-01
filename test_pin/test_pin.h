a4bcb5b87845dd4d9b3d4dd38fff0e036f20d525
pyrex8 <ev64bug@yahoo.com>
2022-03-31 22:56:45 -0700

    working on faster pixel routine

--- test_pin/test_pin.h
+++ test_pin/test_pin.h
@@ -5,8 +5,10 @@
 
 #include <peekpoke.h>
 
+#define STROBE(addr) __asm__ ("sta %w", addr)
+
 #define CASSETTE_OUT_ADDR 0xC020
 #define TEST_PIN_ADDR CASSETTE_OUT_ADDR
-#define TEST_PIN_TOGGLE POKE(TEST_PIN_ADDR, 0)
+#define TEST_PIN_TOGGLE STROBE(TEST_PIN_ADDR)
 
 #endif /* __TEST_PIN_H__ */
